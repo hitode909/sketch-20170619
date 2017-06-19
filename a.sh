cd2084dc6fa05cc69b1c14586b37d796be13990e
hitode909 <hitode909@gmail.com>
2017-06-19 20:42:48 +0900

    loop

--- a.sh
+++ a.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -ex
 
-URL="http://blog.sushi.money/"
-
 mkdir -p tmp/
-cd chrome-headless-screenshots/
-node index.js --url="$URL" --full --delay=5000 --output "../tmp/output.png"
-cd ../
-node upload.js URL "tmp/output.png"
+
+for URL in $URLS
+do
+  echo "$URL"
+  cd chrome-headless-screenshots/
+  node index.js --url="$URL" --full --delay=5000 --output "../tmp/output.png"
+  cd ../
+  node upload.js URL "tmp/output.png"
+  rm "tmp/output.png"
+done
